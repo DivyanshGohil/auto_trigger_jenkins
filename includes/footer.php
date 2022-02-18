98e33f4f7cdf43c0a19dfaa9811751c3447d4782
DivyanshGohil <divyansh.gohil@gmail.com>
2022-02-18 19:30:45 +0530

    updated 1

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <div class="container">
-    <h5 class="text-center "> 19IT042 @ CHARUSAT
+    <h5 class="text-center "> 19IT042 @ CHARUSAT updated for jenkins
         <?= date('Y') ?></h5>
 </div>
