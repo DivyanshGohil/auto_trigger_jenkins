026bbad3523def1eea04db83f33934af50b91fbc
DivyanshGohil <divyansh.gohil@gmail.com>
2022-02-18 19:26:59 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<div class="container">
+    <h5 class="text-center "> 19IT042 @ CHARUSAT
+        <?= date('Y') ?></h5>
+</div>
---
+++ includes/menu.php
@@ -0,0 +1,18 @@
+<div class="row">
+
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e6e600">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="./index.php">CRUD - OOP</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse d-flex justify-content-end " id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                    <a class="navbar-brand"  href="index.php">Users List</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+</div>
